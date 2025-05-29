e05a829584f0dfb0b29c851a3675dffa003e3008
Santosh Gawande <gawandesantos@gmail.com>
2025-05-29 17:52:48 +0530

    feat(candidates): add Candidate model with fields name, age, gender, email, and phone_number

--- ats/candidates/models.py
+++ ats/candidates/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Candidate(models.Model):
+    GENDER_CHOICES = [("M", "Male"), ("F", "Female"), ("O", "Other")]
+    name = models.CharField(max_length=255)
+    age = models.PositiveSmallIntegerField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
